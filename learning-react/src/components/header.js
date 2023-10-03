eb1e2faa6cfb983ab05c4034685c288dc28dfbaa
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-10-02 20:31:26 -0700

    add proj

---
+++ learning-react/src/components/header.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+// let styles = {
+//     header: {
+//         background: '#03a9fa'
+//     },
+//     logo: {
+//         color: '#fff',
+//         fontFamily: 'Anton',
+//         textAlign: 'center'
+//     }
+// }
+
+class Header extends Component {
+
+    render(){
+        return (
+            <>
+                <header>
+                    <div className="logo">Logo</div>
+                    <input/>
+                </header>
+            </>
+        )
+    }
+ };
+
+export default Header;
---
+++ learning-react/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/styles.css';
+
+import Header from './components/header';
+
+const App = () => {
+    return (
+        <div className="hey">
+            < Header />
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
