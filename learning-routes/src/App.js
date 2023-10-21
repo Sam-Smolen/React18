98b7ae2709c47fe52d49f0742f7d122e03207fc5
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-10-21 13:53:33 -0700

    add proj

---
+++ learning-routes/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profile';
+
+const App = () => {
+
+    return (
+        <BrowserRouter>
+            <header>
+                Header
+            </header>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='posts' element={<Posts/>}/>
+                <Route path='profile' element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
