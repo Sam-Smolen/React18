84cd17e68baeb1298326b8a05058f027103321de
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-10-26 11:56:54 -0700

    add transitions

--- transitions/src/components/Transition.js
+++ transitions/src/components/Transition.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { Transition } from 'react-transition-group'
+
 const TransitionComp = () => {
-  return <div>Transition</div>;
+  let [show, setShow] = useState(true);
+
+  const toggleDiv = () => {
+    show ? setShow(false) : setShow(true)
+  }
+
+  return (
+    <>
+    <Transition
+    in={show}
+    timeout={{
+      enter: 2000,
+      exit: 50
+    }}
+    // enter={false}
+    // exit={false}
+    onEnter={(node)=> {
+      console.log('ENTER')
+    }}
+    onExit={(node)=> {
+      console.log('Exit')
+    }}
+    >
+      {state => 
+      <div className={`square square-${state}`}>
+        {`square square-${state}`}
+      </div>
+      }
+    </Transition>
+    <button className="btn btn-primary" onClick={toggleDiv}>Toggle show</button>
+    </>
+  )
 };
 
 export default TransitionComp;
